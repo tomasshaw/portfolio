11818017ea494ee210f90a259d5e9c9dc67cbcdb
Thomas Shaw <tomasshaw@outlook.com>
2022-11-11 00:40:55 -0300

    feat(product): add background image update on scroll

--- app/(product)/bandas/page.tsx
+++ app/(product)/bandas/page.tsx
@@ -1,16 +1,54 @@
 "use client";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import styles from "./page.module.css";
 import { BackgroundContext } from "@/backgroundContext";
 import { bandas, TProducts } from "@/constants";
 
-export default function Page() {
+const useBackgroundPictureUpdater = () => {
   const { setImage } = useContext(BackgroundContext);
 
   useEffect(() => {
     setImage(bandas[0].image);
     return () => setImage(null);
   }, [setImage]);
+
+  return [setImage];
+};
+
+const useIntersectionObserver = () => {
+  const [setImage] = useBackgroundPictureUpdater();
+  const refs = useRef<Array<HTMLDivElement>>([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setImage(refs.current[entry.target.id].dataset.image);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    Object.values(refs.current).forEach((ref) => {
+      observer.observe(ref);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [setImage]);
+
+  return [refs];
+};
+
+export default function Page() {
+  const [setImage] = useBackgroundPictureUpdater();
+  const [productsRef] = useIntersectionObserver();
+  // const productsRef = useRef<Array<HTMLDivElement>>([]);
 
   const handleOnClick = (image: string) => {
     setImage(image);
@@ -21,6 +59,9 @@ export default function Page() {
       {bandas.map((banda) => (
         <ProductItem
           key={banda.title}
+          wrapperRef={(el: HTMLDivElement) =>
+            (productsRef.current[banda.id] = el)
+          }
           product={banda}
           onClick={handleOnClick}
         />
@@ -43,14 +84,18 @@ export default function Page() {
 const ProductItem = ({
   product,
   onClick,
+  wrapperRef,
 }: {
   product: TProducts;
   onClick: any;
+  wrapperRef: any;
 }) => (
   <div
     className={styles.productItem}
     id={product.id}
     onClick={() => onClick(product.image)}
+    ref={wrapperRef}
+    data-image={product.image}
   >
     <h2>{product.title}</h2>
     <h5>{product.subtitle}</h5>
